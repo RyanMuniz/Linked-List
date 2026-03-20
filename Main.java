3c116f95008154c6639d958fc6eccfec019826d6
Ryan Muniz Pagan <95103856+RyanMuniz@users.noreply.github.com>
2026-03-19 21:37:11 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Name: Ryan Muniz
+// Email: rmuniz15@student.cnm.edu
+// Class: CSCI 2260: Section R01
+// Assignment: Week 8: Linked List
+// Purpose: 
+// FileName: "Main.java"
+// Date: March 19, 2026
+
+public class Main {
+    
+}
