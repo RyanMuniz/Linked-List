b9b6f16b4a1fd7df52951881235ad5ea197b4034
Ryan Muniz Pagan <95103856+RyanMuniz@users.noreply.github.com>
2026-03-19 22:44:00 -0600

    Update SortedTreeSetInterface.java

--- SortedTreeSetInterface.java
+++ SortedTreeSetInterface.java
@@ -6,6 +6,16 @@
 // FileName: "SortedTreeSetInterface.java"
 // Date: March 19, 2026
 
-public class SortedTreeSetInterface {
-    
+public interface SortedTreeSetInterface {
+    public Person getPerson();
+
+    public boolean hasLeft();
+    public void setLeft(SortedTreeSet left);
+    public SortedTreeSet getLeft();
+
+    public boolean hasRight();
+    public void setRight(SortedTreeSet right);
+    public SortedTreeSet getRight();
+
+    public void add(Person p);
 }
